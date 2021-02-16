e026fd31edc288de071edbdcc02c9964cf50248c
michaellangbein <michael_langbein@hotmail.de>
2021-02-16 18:36:42 +0100

    DOM over AR working

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,57 @@
-import { AmbientLight, BoxGeometry, DirectionalLight, Mesh, MeshBasicMaterial, MeshPhongMaterial, PerspectiveCamera, RingGeometry, Scene, WebGLRenderer, XRFrame, XRHitTestSource, XRReferenceSpace } from "three";
-import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
+import { CylinderGeometry, HemisphereLight, Mesh, MeshPhongMaterial, Scene, WebGLRenderer, XRFrame } from 'three';
+import { PerspectiveCamera } from 'three';
+import { ARButton } from 'three/examples/jsm/webxr/ARButton';
 
 
 
-/**
- * https://github.com/mrdoob/three.js/blob/master/examples/webxr_ar_hittest.html
- * 
- * 
- * WebXR Concepts
- *  - session
- *  - referenceSpace
- *  - controller
- */
-
-
-
-
-const canvasContainer = document.getElementById('canvasContainer') as HTMLDivElement;
-const gameCanvas = window;
-gameCanvas.addEventListener('resize', () => {
-    camera.aspect = gameCanvas.innerWidth / gameCanvas.innerHeight;
+const container = document.getElementById('canvasContainer') as HTMLDivElement;
+container.addEventListener('resize', () => {
+    camera.aspect = container.clientWidth / container.clientHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(gameCanvas.innerWidth, gameCanvas.innerHeight);
+    renderer.setSize(container.clientWidth, container.clientHeight);
 });
 
-
-const renderer = new WebGLRenderer({
-    antialias: true,
-    alpha: true
-});
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(gameCanvas.innerWidth, gameCanvas.innerHeight);
-renderer.xr.enabled = true;
-canvasContainer.appendChild(renderer.domElement);
-document.body.appendChild(ARButton.createButton(renderer, {
-    optionalFeatures: [ 'dom-overlay', 'dom-overlay-for-handheld-ar' ],
-	domOverlay: { root: document.body }
-}));
 
 const scene = new Scene();
 
-const camera = new PerspectiveCamera(50, 3/2, 0.01, 100);
-camera.position.set(0, 1.6, 0);
-camera.lookAt(0, 0, 0);
+const camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);; 
 
-const cube = new Mesh(
-    new BoxGeometry(1, 1, 1),
-    new MeshPhongMaterial({
-        color: 'red'
-    })
-);
-cube.position.set(0, 1.6, -2);
-scene.add(cube);
+const renderer = new WebGLRenderer({ antialias: true, alpha: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.xr.enabled = true;
+container.appendChild(renderer.domElement);
+
+document.body.appendChild(ARButton.createButton(renderer, {
+	optionalFeatures: [ 'dom-overlay', 'dom-overlay-for-handheld-ar' ],
+	domOverlay: { root: document.body },
+}));
 
 
-const light = new DirectionalLight();
-light.translateX(2);
-light.translateY(2);
+
+const light = new HemisphereLight(0xffffff, 0xbbbbff, 1);
+light.position.set(0.5, 1, 0.25);
 scene.add(light);
-
-
-function doRender(timeStamp:  number, frame: XRFrame | undefined) {
-    if (frame) {
-        cube.rotateX(0.01);
-        cube.rotateY(0.01);
-        renderer.render(scene, camera);
-    }
-}
-renderer.setAnimationLoop(doRender);
 
 
 
 const controller = renderer.xr.getController(0);
-controller.addEventListener( 'select', (args) => {
-    console.log(args);
+controller.addEventListener('select', () => {
+    const geometry = new CylinderGeometry(0, 0.05, 0.2, 32).rotateX(Math.PI / 2);
+    const material = new MeshPhongMaterial({ color: 0xffffff * Math.random() });
+    const mesh = new Mesh(geometry, material);
+    mesh.position.set(0, 0, - 0.3).applyMatrix4(controller.matrixWorld);
+    mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+    scene.add(mesh);
 });
 scene.add(controller);
 
-// const reticle = new Mesh(
-//     new RingGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
-//     new MeshBasicMaterial()
-// );
-// reticle.matrixAutoUpdate = false;
-// reticle.visible = false;
-// scene.add(reticle);
 
 
-// const referenceSpace = renderer.xr.getReferenceSpace();
-
-// function getHitTestSource(): Promise<XRHitTestSource | void> {
-//     const session = renderer.xr.getSession();
-//     return session.requestReferenceSpace('viewer').then((referenceSpace: XRReferenceSpace) => {
-//         session.requestHitTestSource({space: referenceSpace}).then((source: XRHitTestSource) => {
-//            return source; 
-//         });
-//     });
-// }
 
 
-// const r = getHitTestSource().then(v => {
-//     if (v) {
-//         const hitTestResults = frame
-//     }
-// })
+
+renderer.setAnimationLoop((timestamp: number, frame: XRFrame | undefined) => {
+    renderer.render(scene, camera);
+});
+
